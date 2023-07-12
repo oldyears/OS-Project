dda5c177d514fcaa8a7c9587376b00db7b9d5c9e
oldyear <leandgui99@outlook.com>
2023-07-12 21:10:50 +0800

    Add File

---
+++ user/xargs.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/param.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+int main(int argc,char** argv)
+{
+    // 首先处理
+    
+}
